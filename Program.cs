76c16d40ef562a15724f22a7efd5e01d9a08a877
Webermanoel <webermanoel22@gmail.com>
2025-03-31 21:53:55 -0400

    Subindo primeira versão do código

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Lista
+{
+    static void Main()
+    {
+        List<string> fruta = new List<string> {"Banana", "Maçã", "Laranja", "Manga",
+        "Melancia", "Abacaxi"};
+
+        Console.WriteLine("\r\nDeseja comprar mais alguma fruta para estoque?\r\n");  
+        
+        string novafruta = Console.ReadLine();
+        
+        Console.WriteLine($"\r\nA fruta: {novafruta} foi adicionada ao estoque\r\n");
+        
+        fruta.Add( novafruta );
+
+        Console.WriteLine("\r\nFrutas em estoque: " + string.Join(", ", fruta));
+
+    }
+}
